ac27a519aacccb5ad19c8498988184c288d619ed
seokjin0414 <sars21@hanmail.net>
2023-08-21 14:42:08 +0900

    first

---
+++ src/test/java/shin/test_proxy/TestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package shin.test_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
